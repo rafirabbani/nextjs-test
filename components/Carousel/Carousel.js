6283da4180db4cd4c15d196c4efd66519edfb5a8
Yusuf Rafi Rabbani <rafirabbani103@gmail.com>
2022-12-15 18:04:59 +0700

    add carousel component

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,1 +1,24 @@
 // to do: create carousel component
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Image from 'next/image';
+import styles from './Carousel.module.css';
+
+export default function CarouselComponent({images}) {
+    // console.log(images);
+    return (   
+        <Carousel showThumbs={false}>
+            {
+                images && images.map((data, key) => {
+                    return (
+                        <div key={key} className={styles.imageContainer}>
+                            <img src={`https://storage.googleapis.com/${data.bucketName}/${data.fileName}`}
+                                alt={data.fileName} className={styles.image}
+                            />
+                        </div>
+                    )
+                })
+            }
+        </Carousel>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,34 @@
 import Image from 'next/image'
 import Layout from '../components/Layout/Layout'
 import styles from '../styles/Home.module.css'
+import Carousel from '../components/Carousel/Carousel'
 
 export default function Home({images, size}) {
   // to do design landing page
   return (
     <Layout size={size}>
-        {/* Profile */}
-        <div className={styles.container}>
-              <div className={styles.profile}>
-                  <div className={styles.left}>Left</div>
-                  <div className={styles.right}>Right</div>
-              </div>
-        </div>
-        {/* Gallery */}
-        <div className={styles.gallery}>
-          {images.files && images.files.map((data, index) => {
-            return (
-              <Image key={index} src={`https://storage.googleapis.com/${data.bucketName}/${data.fileName}`} alt={data.fileName} 
-                width={100} height={150}
-                className={styles.image}
-              />
-            );  
-          })}
-        </div>
+      {/* Carousel */}
+      <div className={styles.carouselContainer}>
+        <Carousel images={images.files && images.files.slice(0,6)}/>
+      </div>
+      {/* Profile */} 
+      <div className={styles.container}>
+            <div className={styles.profile}>
+                <div className={styles.left}>Left</div>
+                <div className={styles.right}>Right</div>
+            </div>
+      </div>
+      {/* Gallery */}
+      <div className={styles.gallery}>
+        {images.files && images.files.map((data, index) => {
+          return (
+            <Image key={index} src={`https://storage.googleapis.com/${data.bucketName}/${data.fileName}`} alt={data.fileName} 
+              width={100} height={150}
+              className={styles.image}
+            />
+          );  
+        })}
+      </div>
     </Layout>
   );
 }
